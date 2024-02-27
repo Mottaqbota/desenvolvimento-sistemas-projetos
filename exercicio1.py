9bc19b676120c3061c235833f8f2214b09ec23be
Motta <125099026+Mottaqbota@users.noreply.github.com>
2024-02-27 14:00:49 -0300

    Create exercicio1.py

---
+++ exercicio1.py
@@ -0,0 +1,11 @@
+nome = 'João Motta'
+idade = 16
+
+print('Me chamo', nome, 'e a idade é', idade, 'anos!')
+
+item = 'Abacaxi'
+preco_item = 10.99
+message = 'Vou comprar {}, que custa {} reais'
+
+# Mostrando as variáveis dentro da frase
+print(message.format(item, preco_item))a
